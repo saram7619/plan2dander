888b906f223eb3d4c4c952556638772e5f529a48
Sara McClelland <saramcclelland@icloud.com>
2023-12-06 17:50:44 +0000

    added route to middleware and sign-in and sign-up buttons

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,9 @@
+
+
+const NotesPage = () => {
+  return (
+    <div>Here are where the notes will be</div>
+  )
+}
+
+export default NotesPage
---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@clerk/nextjs"
+
+const SignInPage = () => {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+        <SignIn appearance={{ variables: { colorPrimary: '#0F172A' } }}/>
+    </div>
+  )
+}
+
+export default SignInPage
